b9b7643132763ded61632debc00d36385198af1d
Valencia <davidalencia@gmail.com>
2017-04-23 20:32:29 -0500

    agregue palabras y funcion para sacar una al azar

--- principal.php
+++ principal.php
@@ -1,5 +1,14 @@
 <?php
+include('funciones.php');
+
+$palabras = array( "crema", "saco", "examen", "volumen", "origen", "salas",
+  "trenes", "abanderado", "abanico", "acuarela", "acusado", "anulares",
+  "arrecife", "avispa", "banquero", "cilindro", "cintura", "cuaderno",
+  "elefante", "elemento", "lechuga", "martes", "resortes", "tijeras",
+  "Zapatos" );
+
+
+  echo "<br/>".azar($palabras)."<br/>";
 
 
 ?>
- 
