4acd97fa86efc53bb65d30f3610a0127e3beec34
Roberto Marcelo Ramirez Aguilar <Roberto Marcelo Ramirez Aguilar>
2017-04-23 22:03:14 -0500

    con opciones para seleccionar

---
+++ ahorcado/funciones.php
@@ -0,0 +1,16 @@
+<?php
+//devuelve una palabra al azar ademas de retirarla
+//cambiando el # de indoces en el array
+function azar(&$array){
+  $largo=count($array);
+  $n=rand(0,$largo-1);
+  $tmp=$array[$n];
+
+  for ($alfa=$n; $alfa < $largo-2; $alfa++)
+    $array[$alfa]=$array[$alfa+1];
+
+  unset($array[$largo-1]);
+  return $tmp;
+}
+
+?>
--- principal.php
+++ principal.php
@@ -1,14 +1,31 @@
 <?php
 include('funciones.php');
 
+echo 	"<!DOCTYPE HTML>
+			<html lang='es'>
+				<head>";
+	echo 			"<tiltle></title>
+					<meta charset='UTF-8'/>
+					<link rel='stylesheet' type='text/css' href='estilado.css'/>";
+	echo		"</head>
+				<body>";
+	echo 			"<form method='POST' action='principal.php'>
+						<h1>Selecciona el tema del que desees adivinar</h1><br/>
+						<select name='tema'>
+							<option value='' selected>--Seleccionar--</option>
+							<option value='comidas'>Comidas</option>
+							<option value='calzado'>Calzado</option>
+							<option value='vida marina'>Vida marina</option>
+						</select>
+						<input type='submit' value='Establecer tema'>
+					</form>";
+	echo 		"</body>
+			</html>";
 $palabras = array( "crema", "saco", "examen", "volumen", "origen", "salas",
   "trenes", "abanderado", "abanico", "acuarela", "acusado", "anulares",
   "arrecife", "avispa", "banquero", "cilindro", "cintura", "cuaderno",
   "elefante", "elemento", "lechuga", "martes", "resortes", "tijeras",
   "zapatos" );
-
-
-  echo "<br/>".azar($palabras)."<br/>";
-
-
+  
+	echo "<br/>".azar($palabras)."<br/>";
 ?>
